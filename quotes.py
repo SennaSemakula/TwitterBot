2410903b12a6b469bb9716b68d86831724cf179c
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-06-20 19:53:29 +0100

    change quotes position

--- quotes.py
+++ quotes.py
@@ -1,5 +1,6 @@
 QUOTES = [
     "Treat global variables like someone has a knife to your throat.",
+        "Hate working with undocumented code? Get into the habit of writing comments to enforce better habits for others to adopt.",
     "Learn to start one programming language and stick with that. Build a solid foundation first.",
     "Don't choke up on syntax. It hardly matters. Try to work on your logics and patterns.",
     "Use a Coding Standard to increase readibility of code.",
@@ -8,7 +9,6 @@ QUOTES = [
     "Set some time aside to work on technical debt.",
     "When naming variables, use meaningful names.",
     "Use version control software to help manage your source code better.",
-    "Hate working with undocumented code? Get into the habit of writing comments to enforce better habits for others.",
     "The most disastrous thing that you can ever learn is your first programming language.",
     "In your own time, work on your own code. Do something fun. Do something pointless. Abandon with abandon. There is no pressure to finish anything other than the pressure you put on yourself",
     "Don’t rely on coffee or energy drinks to keep you ‘buzzing’. This is a clear indication that you’re too tired to do good work.",
