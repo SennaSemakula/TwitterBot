99bed4a858e2a764c18d3c4ca9e0bceed50f82a7
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-06-20 18:14:42 +0100

    add quotes

---
+++ quotes.py
@@ -0,0 +1,11 @@
+QUOTES = [
+    "Treat global variables like someone has a knife to your throat.",
+    "Use a Coding Standard to increase readibility of code.",
+    "Write Useful Comments to assist with collaboration.",
+    "Stuck on a problem? Write it out. It'll open up different angles that you couldn't see before.",
+    "Set some time aside to work on technical debt.",
+    "When naming variables, use meaningful names.",
+    "Use version control software to help manage your source code better.",
+    "Hate working with undocumented code? Get into the habit of writing comments to enforce better habits for others.",
+    "The most disastrous thing that you can ever learn is your first programming language.",
+]
