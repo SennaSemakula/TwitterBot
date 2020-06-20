b0c120568ee8069997bcd59635cb4e947df61a53
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-06-20 18:13:33 +0100

    add twitter bot

---
+++ tweet.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import tweepy
+import json
+from quotes import QUOTES
+
+HASH_TAGS = ["#CodeNewbie", "#100daysofcode", "#tech", "#python", "#javascript"]
+
+auth = tweepy.OAuthHandler(os.environ["CONSUMER_API_KEY"], os.environ["CONSUMER_API_SECRET"])
+auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
+
+api = tweepy.API(auth)
+
+a_file = open("counter.json", "r")
+day = json.load(a_file)['day']
+a_file.close()
+
+tweet = f"{QUOTES[day]}\n"
+for tag in HASH_TAGS:
+    tweet += f"{tag} "
+print(tweet)
+
+new_day = day + 1
+
+with open("counter.json", "w+") as f_obj:
+    f_obj.write(json.dumps({"day": new_day}))
+
+# api.update_status("Testing api \n #london")
+
