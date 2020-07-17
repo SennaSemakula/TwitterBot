c0465ad742923a36b9373940b29ebadf211c795a
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-07-17 22:54:21 +0100

    add jokes and argparse

---
+++ jokes.py
@@ -0,0 +1,14 @@
+JOKES = [
+    "Why did the programmer quit his job? Because he didn’t get arrays",
+    "How many programmers does it take to change a light bulb?\nNone. It's a hardware problem.",
+    "CD-ROM: Consumer Device, Rendered Obsolete in Months",
+    "Debugging is like being the detective in a crime drama where you are also the murderer.",
+    "There are only two kinds of people in this world: those who know binary and those who don’t.",
+    "WINDOWS: Will Install Needless Data On Whole System",
+    "Me: Will you be my Valentine?\nGirl: No way\nMe: sudo will you be my Valentine?\nLinux: Yes..yes..yes!.",
+    "Friend: What is your address?\nMe: 173.168.15.10\Friend: No, your local address\nMe: 127.0.01\Friend: I mean your physical address\nMe: 29:01:38:62:31:58",
+    "An SQL query goes into a bar, walks up to two tables and asks: Can I join you?",
+    "Question: Do you know why Facebook went public?\n\nAnswer: They couldn't figure out the privacy settings.",
+    "There are two ways to write error-free programs; only the third one works",
+    "I used to make IPv4 jokes - but I ran out.",
+]
--- tweet.py
+++ tweet.py
@@ -2,7 +2,22 @@
 import os
 import tweepy
 import json
+import argparse
 from quotes import QUOTES
+from jokes import JOKES
+
+parser = argparse.ArgumentParser(description="Script to tweet")
+parser.add_argument('--type', help="Type of tweet. Can be motivational or joke.")
+args = parser.parse_args()
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+if args.type == "motivation":
+    data = QUOTES
+    config = "{0}/counter.json".format(DIR_PATH)
+elif args.type == "joke":
+    data = JOKES
+    config = "{0}/jokes_counter.json".format(DIR_PATH)
 
 HASH_TAGS = ["#CodeNewbie", "#100daysofcode", "#tech", "#python", "#javascript"]
 
@@ -11,18 +26,17 @@ auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRE
 
 api = tweepy.API(auth)
 
-a_file = open("counter.json", "r")
+a_file = open(config, "r")
 day = json.load(a_file)['day']
 a_file.close()
 
-tweet = "{0}\n".format(QUOTES[day])
+tweet = "{0}\n".format(data[day])
 for tag in HASH_TAGS:
     tweet += f"{tag} "
 print(tweet)
 
 new_day = day + 1
-
-with open("counter.json", "w+") as f_obj:
+with open(config, "w+") as f_obj:
     f_obj.write(json.dumps({"day": new_day}))
 
 api.update_status(tweet)
